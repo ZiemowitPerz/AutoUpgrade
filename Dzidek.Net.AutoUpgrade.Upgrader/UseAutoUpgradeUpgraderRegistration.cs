0ae45438e74393b6d3384cb7e77401872201f0f9
a.chwilkowski <a.chwilkowski@integra.com.pl>
2025-09-12 15:33:36 +0200

    added serilog

--- Dzidek.Net.AutoUpgrade.Upgrader/UseAutoUpgradeUpgraderRegistration.cs
+++ Dzidek.Net.AutoUpgrade.Upgrader/UseAutoUpgradeUpgraderRegistration.cs
@@ -2,6 +2,7 @@
 using Dzidek.Net.AutoUpgrade.Upgrader.FileSystemWatchers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Dzidek.Net.AutoUpgrade.Upgrader;
 
@@ -10,12 +11,16 @@ public static class UseAutoUpgradeUpgraderRegistration
     public static IHostBuilder UseAutoUpgradeUpgrader(this IHostBuilder hostBuilder,
         AutoUpgradeUpgraderConfiguration configuration)
     {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day, fileSizeLimitBytes: 1L * 1024 * 1024)
+            .CreateLogger(); 
         return hostBuilder.UseAutoUpgrade(configuration.ServiceName, configuration.UpgraderNameSuffix, services =>
         {
             services
                 .AddSingleton<IFileWatcher, FileWatcher>()
                 .AddSingleton<AutoUpgradeUpgraderConfiguration>(x => configuration)
                 .AddHostedService<UpgraderService>();
-        });
+        }).UseSerilog();
     }
 }
